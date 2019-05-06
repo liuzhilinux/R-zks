fc60228317b63c36aefa2e32f9b61dad3bf2ce5e
ling <i@e7iling.com>
2019-05-06 23:45:59 +0800

    添加依赖文件。

---
+++ vendor/tinymce_4.9.2/plugins.js
@@ -0,0 +1,26 @@
+const default_plugins = [
+    'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+    'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+    'save table contextmenu directionality emoticons template paste textcolor'
+];
+
+/*
+ 备选：
+ fullpage
+ */
+
+const plugins = [
+    'advlist anchor autosave autolink code codesample colorpicker contextmenu',
+    'directionality emoticons fullscreen hr image imagetools insertdatetime link lists media',
+    'nonbreaking noneditable pagebreak paste preview print save searchreplace spellchecker',
+    'tabfocus table template textcolor textpattern visualblocks visualchars wordcount',
+    'charmap'
+];
+
+const pluginx = [
+    'tinydrive a11ychecker advcode advlist anchor  autolink charmap codesample colorpicker',
+    'contextmenu directionality\", \"fullscreen help hr image imagetools insertdatetime link',
+    'linkchecker lists media mediaembed noneditable", "pagebreak powerpaste preview print',
+    'searchreplace table template textcolor textpattern tinymcespellchecker toc',
+    'visualblocks visualchars wordcount formatpainter'
+];
---
+++ vendor/tinymce_4.9.2/toolbar.js
@@ -0,0 +1,21 @@
+const default_toolbar = 'insertfile undo redo |'
+    + ' styleselect | bold italic | alignleft aligncenter alignright alignjustify |'
+    + ' bullist numlist outdent indent | link image | print preview media fullpage |'
+    + ' forecolor backcolor emoticons';
+
+/*
+ 备选：
+ formatselect
+ charmap anchor pagebreak insertdatetime
+ */
+
+const toolbar = [
+    'undo redo | bold italic underline strikethrough |'
+    + ' alignleft aligncenter alignright alignjustify outdent indent |'
+    + ' blockquote removeformat subscript superscript code codesample |'
+    + ' newnote searchreplace preview print fullscreen restoredraft save',
+
+    'bullist numlist | link image media table emoticons insert |'
+    + ' forecolor backcolor formatpainter | styleselect | fontselect | fontsizeselect'
+];
+
